1ff77fbed5d2d6947e5b25798a09c418c89c249c
Heroku <heroku@Herokus-MacBook-Pro-15.local>
2010-02-08 17:48:06 -0800

    get pg template working

--- cookbooks/postgis/recipes/default.rb
+++ cookbooks/postgis/recipes/default.rb
@@ -3,19 +3,19 @@
 # puts "make sure we have enough shmmax"
 # Rush.bash "echo #{shmmax} > /proc/sys/kernel/shmmax"
 
-memory = node["memory"]["shared"].to_i * 1024
+memory = node[:memory][:shared].to_i * 1024
 shared_memory = memory / 3
 current_shared_memory = File.read("/proc/sys/kernel/shmmax").to_i
 
 packages = %w(postgresql-8.4 postgresql-server-dev-8.4 libpq-dev libgeos-dev proj)
 
 packages.each do |p|
-	package p do
-		action :install
-	end
+  package p do
+    action :install
+  end
 end
 
-service "postgresql-8.4"  do
+service "postgresql-8.4" do
   supports :restart => true, :reload => true
   action :enable
 end
@@ -27,8 +27,11 @@ execute "setup-shmmax" do
 end
 
 template "/etc/postgresql/8.4/main/postgresql.conf" do
-   source "postgresql.conf.erb"
-   mode "0644"
-	notifies :restart, resources(:service => "postgresql-8.4")
+  source "postgresql.conf.erb"
+  mode "0644"
+  notifies :restart, resources(:service => "postgresql-8.4")
+	variables(
+		:ram_mb => memory / 1024 / 1024
+	)
 end
 
