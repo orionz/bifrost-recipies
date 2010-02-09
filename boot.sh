862efad2051cede26bf47b56d3de062f5f6a8e4c
Heroku <heroku@Herokus-MacBook-Pro-15.local>
2010-02-08 17:58:33 -0800

    notes in boot.sh

--- boot.sh
+++ boot.sh
@@ -4,3 +4,6 @@ apt-get upgrade -y
 apt-get install ruby irb rubygems ruby-dev git-core -y
 gem install rake chef ohai --no-rdoc --no-ri
 
+# git clone git://github.com/orionz/bifrost-recipies.git
+# /var/lib/gems/1.8/bin/chef-solo -c config.rb -j bifrost.json 
+
