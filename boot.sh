d4b01ce97ff8a2982899902d6fedc6f258c07f7b
Orion Henry <orion@heroku.com>
2010-02-10 16:34:22 -0500

    added postgis package

--- boot.sh
+++ boot.sh
@@ -1,9 +1,31 @@
+#!/bin/bash
 
 apt-get update
 apt-get upgrade -y
-apt-get install ruby irb rubygems ruby-dev libopenssl-ruby git-core -y
+apt-get install git-core xfsprogs mdadm -y
+apt-get install postgresql-8.4 postgresql-server-dev-8.4 libpq-dev libgeos-dev proj -y
+/etc/init.d/postgresql-8.4 stop
+
+### all this does not is write the configuration file
+cd /tmp/
+git clone git://github.com/orionz/bifrost-recipies.git
+cd bifrost-recipies
+apt-get install ruby irb rubygems ruby-dev libopenssl-ruby -y
 gem install rake chef ohai --no-rdoc --no-ri
+/var/lib/gems/1.8/bin/chef-solo -c config.rb -j bifrost.json
+dpkg -i postgis_1.4.1-src-1_i386.deb
+####
 
-# git clone git://github.com/orionz/bifrost-recipies.git
-# /var/lib/gems/1.8/bin/chef-solo -c config.rb -j bifrost.json 
+mdadm --assemble /dev/md0 /dev/sde2 /dev/sde1
+blockdev --setra 65536 /dev/md0
+mkdir /database
+mkdir /wal
+mount -L /database /database
+mount -L /wal /wal
 
+/etc/init.d/postgresql-8.4 start
+
+#TODO: manually  mount drives
+#	mdadm --assemble /dev/md0 /dev/sde1 /dev/sde2
+#	mount -L /wal /wal
+#	mount -L /database /database
--- cookbooks/postgis/recipes/default.rb
+++ cookbooks/postgis/recipes/default.rb
@@ -1,24 +1,7 @@
 
-# shmmax = kb * 1024 / 3 # one third of ram
-# puts "make sure we have enough shmmax"
-# Rush.bash "echo #{shmmax} > /proc/sys/kernel/shmmax"
-
 memory = node[:memory][:total].to_i * 1024
 shared_memory = memory / 3
 current_shared_memory = File.read("/proc/sys/kernel/shmmax").to_i
-
-packages = %w(postgresql-8.4 postgresql-server-dev-8.4 libpq-dev libgeos-dev proj)
-
-packages.each do |p|
-  package p do
-    action :install
-  end
-end
-
-service "postgresql-8.4" do
-  supports :restart => true, :reload => true
-  action :enable
-end
 
 execute "setup-shmmax" do
   command "echo #{shared_memory} > /proc/sys/kernel/shmmax"
@@ -29,7 +12,6 @@ end
 template "/etc/postgresql/8.4/main/postgresql.conf" do
   source "postgresql.conf.erb"
   mode "0644"
-  notifies :restart, resources(:service => "postgresql-8.4")
 	variables(
 		:ram_mb => memory / 1024 / 1024
 	)
