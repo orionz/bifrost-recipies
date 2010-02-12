d227755157dde9ffe53cc5c28c0b81abe963ff71
Orion Henry <orion@heroku.com>
2010-02-12 14:12:18 -0500

    64 bit package

--- boot.sh
+++ boot.sh
@@ -13,14 +13,21 @@ cd bifrost-recipies
 apt-get install ruby irb rubygems ruby-dev libopenssl-ruby -y
 gem install rake chef ohai --no-rdoc --no-ri
 /var/lib/gems/1.8/bin/chef-solo -c config.rb -j bifrost.json
+echo 'host all all 0.0.0.0/0 md5' >> /etc/postgresql/8.4/main/pg_hba.conf 
 dpkg -i postgis_1.4.1-src-1_i386.deb
 ####
 
-mdadm --assemble /dev/md0 /dev/sde2 /dev/sde1
+echo "waiting for devices to attach"
+while [ $(blkid /dev/sde1 /dev/sdf* | wc -l) != 9 ] ; do sleep 1; done
+
+echo -n "assembling raid"
+while !  mdadm --assemble /dev/md0 /dev/sdf* ; do echo -n . ; sleep 5 ; done
 blockdev --setra 65536 /dev/md0
 mkdir /database
-mkdir /wal
 mount -L /database /database
+echo "done"
+
+mkdir /wal
 mount -L /wal /wal
 
 /etc/init.d/postgresql-8.4 start
